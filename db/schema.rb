0b6936d1e48d2d728ca2d82d84d90e84f384ad69
soumen <funmon.rapbit@gmail.com>
2020-02-18 13:51:06 +0000

    set preparation

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_120115) do
+ActiveRecord::Schema.define(version: 2020_02_09_124608) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "buy_date"
     t.string "status"
     t.bigint "user_id"
-    t.string "buy_date"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
